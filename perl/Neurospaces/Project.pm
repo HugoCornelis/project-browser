57b5d57d7cc04727e57e4e46fe2277f14416fe12
Hugo Cornelis <hugo.cornelis@gmail.com>
2009-01-18 11:01:33 +0000

    1. Added functions to create an initial directory layout.  Does not    work yet, does not prefix with the root directory of the project.

--- perl/Neurospaces/Project.pm
+++ perl/Neurospaces/Project.pm
@@ -29,6 +29,73 @@ use strict;
 use Neurospaces::Project::Modules::Morphology;
 
 
+our $project_directories
+    = [
+       {
+	description => 'directory with subdirectories with reconstructed neuronal morphologies',
+	directory => 'morphologies',
+	related => [
+		    {
+		     description => 'morphology_groups',
+		     directory => 'morphology_groups',
+		    },
+		    {
+		     description => 'network_groups',
+		     directory => 'network_groups',
+		    },
+		    {
+		     description => 'kinetic_groups',
+		     directory => 'kinetic_groups',
+		    },
+		   ],
+       },
+       {
+	description => 'database that groups reconstructed neuronal morphologies for further analysis',
+	directory => 'morphology_groups',
+       },
+       {
+	description => 'database with simulation definitions',
+	directory => 'modules',
+       },
+       {
+	description => 'free style pictures',
+	directory => 'pictures',
+       },
+       {
+	description => 'scripts for analysis',
+	directory => 'scripts',
+       },
+       {
+	description => 'output of analysis scripts',
+	directory => 'scripts/output',
+       },
+       {
+	description => 'colormaps of morphologies',
+	directory => 'colormaps',
+       },
+       {
+	description => 'narrative overview of the project',
+	directory => 'narrative_components',
+       },
+       {
+	description => 'summary notes of project progress',
+	directory => 'summary',
+       },
+       {
+	description => 'papers related to this project',
+	directory => 'papers',
+       },
+       {
+	description => 'models private to this project',
+	directory => 'models',
+       },
+       {
+	description => 'project component replicator',
+	directory => 'replicator',
+       },
+      ];
+
+
 sub all_morphologies
 {
     my $self = shift;
@@ -53,6 +120,42 @@ sub all_morphologies
 }
 
 
+sub create
+{
+    my $self = shift;
+
+    # loop through all project directories
+
+    foreach my $project_directory (@$project_directories)
+    {
+	my $directory = $project_directory->{directory};
+
+	# create a directory
+
+	if (!-d $directory)
+	{
+	    mkdir $directory;
+	}
+
+	# create the default descriptor
+
+	my $descriptor_filename = "$directory/descriptor.yml";
+
+	if (!-r $descriptor_filename)
+	{
+	    my $descriptor
+		= {
+		   description => $project_directory->{description},
+		  };
+
+	    use YAML "DumpFile";
+
+	    DumpFile($descriptor_filename, $descriptor);
+	}
+    }
+}
+
+
 sub load
 {
     my $package = shift;
@@ -74,9 +177,9 @@ sub load
 	= Neurospaces::Project->new
 	    (
 	     {
-	      config => $project_config,
+# 	      config => $project_config,
 	      name => $options->{name},
-	      root => "$neurospaces_config->{project_browser}->{root_directory}",
+# 	      root => "$neurospaces_config->{project_browser}->{root_directory}",
 	     },
 	    );
 }
@@ -88,8 +191,38 @@ sub new
 
     my $options = shift || {};
 
+    if (!defined $options->{name})
+    {
+	die "$0: need a project name";
+    }
+
+    use YAML 'LoadFile';
+
+    my $neurospaces_config;
+
+    my $neurospaces_config_filename = '/etc/neurospaces/project_browser/project_browser.yml';
+
+    if (-r $neurospaces_config_filename)
+    {
+	$neurospaces_config = LoadFile($neurospaces_config_filename);
+    }
+    else
+    {
+	$neurospaces_config
+	    = {
+	       project_browser => {
+				   root_directory => '/var/neurospaces/simulation_projects/',
+				  },
+	      };
+    }
+
+    my $project_config = LoadFile("$neurospaces_config->{project_browser}->{root_directory}/$options->{name}/descriptor.yml");
+
     my $self
 	= {
+	   config => $project_config,
+	   name => $options->{name},
+	   root => "$neurospaces_config->{project_browser}->{root_directory}",
 	   %$options,
 	  };
 
